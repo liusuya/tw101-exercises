cef8a6c295703f507d4bceafd40d4d82d5270075
Suya Liu <suya1122@gmail.com>
2016-07-20 12:25:16 -0400

    adding ex 1

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -4,6 +4,14 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 public class Main {
     public static void main(String[] args) {
+        int iSum = 0;
+        int increment = 2;
+        int iMax = 100;
+        for (int num = 1; num < iMax; num+=increment) {
+            System.out.println(num);
+            iSum+=num;
+        }
+        System.out.println(iSum);
 
     }
 }
