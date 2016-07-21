b38f5a76fa9f0bb2173e45967561a0353626b0fb
Suya Liu <suya1122@gmail.com>
2016-07-20 21:52:18 -0400

    exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    int incInstances = 0;
+
+    public void incremement() {
+        incInstances++;
+    }
+
+    public void total() {
+        System.out.println("The increment method was called " + incInstances + " times.");
+    }
 }
