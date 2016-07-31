a434c050cf93bd406b7178f401d27a017d7274c4
suya1122@gmail.com <suya1122@gmail.com>
2016-07-31 00:33:15 -0400

    ex9 done

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,48 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
     private String name;
+    private Node leftChild;
+    private Node rightChild;
 
     public Node(String name) {
         this.name = name;
     }
 
     public void add(String nameOfNewNode) {
+        if (nameOfNewNode.compareTo(name) < 0) {
+            //the new name is lexicographically before the current node's name
+            if (leftChild == null) {
+                leftChild = new Node(nameOfNewNode);
+            }
+            else {
+                leftChild.add(nameOfNewNode);
+            }
+        }
+        else {
+            //the new name is the same as the current node's name or after it
+            if (rightChild == null) {
+                rightChild = new Node(nameOfNewNode);
+            }
+            else {
+                rightChild.add(nameOfNewNode);
+            }
+        }
     }
 
     public List<String> names() {
-        return null;
+        ArrayList<String> list = new ArrayList<String>();
+
+        if (leftChild != null) {
+            list.addAll(leftChild.names());
+        }
+        list.add(name);
+        if (rightChild != null) {
+            list.addAll(rightChild.names());
+        }
+        return list;
     }
 }
